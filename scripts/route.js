d7849f16ddef54cd6022f1783368d22fda5b1aeb
kenrickchen <kenrickchen0@gmail.com>
2024-03-29 18:19:48 -0400

    nodejs init

--- scripts/route.js
+++ scripts/route.js
@@ -1,19 +1,32 @@
-import TOKEN from "./token";
+var map = L.map('map').setView([39.95, -75.165], 12);
 
-var options = {
-    searchOptions: {
-      key: TOKEN,
-      language: "en-US",
-      limit: 5,
-    },
-    autocompleteOptions: {
-      key: TOKEN,
-      language: "en-US",
-    },
+$.getJSON("https://opendata.arcgis.com/datasets/b5f660b9f0f44ced915995b6d49f6385_0.geojson", function(json) {
+  L.geoJson(json, {style: style}).addTo(map);
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+function getColor(type) {
+  return type === "Sharrow" ? "#ff0000" :
+         type === "Conventional" ? '#ff7b00' :
+         type === "Paint Buffered"  ? '#ffc800' :
+         type === "Separated Bike Lane"  ? '#00ff00' :
+         type === "Two Way Separated Bike Lane"  ? '#0000ff' :
+                  '#ff00ff';
 }
 
-var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
-var map = tt.map({
-    key: TOKEN,
-    container: "map",
-})
+function style(feature) {
+  return {
+    color: getColor(feature.properties.TYPE),
+    opacity: 0.5
+  };
+}
+
+var routingControl = new L.Routing.Control({
+    routeWhileDragging: true,
+    geocoder: L.Control.Geocoder.nominatim()
+});
+
+routingControl.addTo(map);
