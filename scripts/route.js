90c9e741d1cb2dbe10d37189ae55d99cd23ca2b4
kenrickchen <kenrickchen0@gmail.com>
2024-04-06 00:21:44 -0400

    added google map tiles, added legend

--- scripts/route.js
+++ scripts/route.js
@@ -1,24 +1,54 @@
 let map = L.map('map', {
-  minZoom: 11,
+  minZoom: 11
 }).setView([39.95, -75.165], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+// street: http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}
+// satellite: http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}
+
+L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
 let control = L.Routing.control({
   geocoder: L.Control.Geocoder.photon({
     geocodingQueryParams: {
       lat: "39.95",
-      lon: "-75.165"
+      lon: "-75.165",
     }
   }),
   reverseWaypoints: true,
   router: L.Routing.mapzen('', {
     costing:'bicycle'
   }),
-  formatter: new L.Routing.mapzenFormatter(),
+  formatter: new L.Routing.mapzenFormatter({
+    units: 'imperial'
+  }),
+  lineOptions: {
+    addWaypoints: false
+  },
 }).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
+
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function(map) {
+  let div = L.DomUtil.create('div', 'legend');
+  let types = {
+    "Sharrows": "#fe797b",
+    "Bus Lane": "#ffb750",
+    "Conventional": "#ffea56",
+    "Buffered": "#8fe968",
+    "Protected": "#36cedc"
+  }
+  for (const key in types) {
+    div.innerHTML += '<i style="background:' + types[key] + '"></i> ' + key + '<br>';
+  }
+  return div;
+}
+
+legend.addTo(map);
 
 function createButton(label, container) {
   var btn = L.DomUtil.create('button', '', container);
@@ -28,10 +58,15 @@ function createButton(label, container) {
 }
 
 map.on('click', function(e) {
-
+  console.log(control.getWaypoints());
   let container = L.DomUtil.create('div', 'popup');
   let startButton = L.DomUtil.create('button', '', container);
   startButton.innerHTML = 'Start from this location';
+  let addButton;
+  if (control.getWaypoints()[0].latLng !== undefined) {
+    addButton = L.DomUtil.create('button', '', container);
+    addButton.innerHTML = 'Add location as stop';
+  }
   let destinationButton = L.DomUtil.create('button', '', container);
   destinationButton.innerHTML = 'Go to this location';
 
@@ -45,6 +80,13 @@ map.on('click', function(e) {
     map.closePopup();
   });
 
+  if (addButton !== undefined) {
+    L.DomEvent.on(addButton, 'click', function() {
+      control.spliceWaypoints(control.getWaypoints().length - 1, 0, e.latlng);
+      map.closePopup();
+    })
+  }
+
   L.DomEvent.on(destinationButton, 'click', function() {
     control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
     map.closePopup();
@@ -54,48 +96,46 @@ map.on('click', function(e) {
 function getColor(type) {
   switch(type) {
     case "Sharrow":
-      return "#ff0000";
+      return "#fe797b";
+    case "Bus Bike Lane":
+      return "#ffb750";
     case "Conventional":
-      return "#ff7b00";
+    case "Conventional w Sharrows":
+    case "Dashed Bike Lane":
+    case "Conventional & Dashed Bike Lane":
+    case "Contraflow":
+      return "#ffea56";
     case "Paint Buffered":
-      return "#ffc800";
+    case "Paint Buffered w Conventional":
+    case "Two Way Paint Buffered Bike Lane":
+      return "#8fe968";
     case "Separated Bike Lane":
-      return "#00ff00";
+    case "One Way Separated Bike Lane":
+    case "Two Way Separated Bike Lane":
+      return "#36cedc";
     default:
-      return '#ff00ff';
+      return 'transparent';
   }
 }
 
 function style(feature) {
   return {
     color: getColor(feature.properties.TYPE),
-    opacity: 0.5,
     weight: 5,
+    opacity: 0.6,
     lineCap: "butt"
   }
 }
 
-function onEachFeature(feature, layer) {
-  map.on('zoomend', function() {
-    let currentZoom = map.getZoom();
-    console.log(currentZoom);
-    if (currentZoom < 13) {
-      layer.setStyle({weight: 2});
-    } else if (currentZoom < 15) {
-      layer.setStyle({weight: 5});
-    } else {
-      layer.setStyle({weight: 7});
-    }
-  });
-
-  
-}
+map.on('zoomend', function() {
+  let currentZoom = map.getZoom();
+  geojson.setStyle({weight: currentZoom-9, opacity: 1-((currentZoom-5)/20)})
+});
 
 let geojson;
 
-$.getJSON("https://opendata.arcgis.com/datasets/b5f660b9f0f44ced915995b6d49f6385_0.geojson", function(json) {
+$.getJSON("./resources/bike_network.geojson", function(json) {
   geojson = L.geoJson(json, {
     style: style,
-    onEachFeature: onEachFeature
   }).addTo(map);
 });
